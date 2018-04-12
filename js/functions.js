dca796fc78964bb5c09cdf1b96009a6b64499c89
jaime <design.jumi@gmail.com>
2018-04-11 23:08:24 -0700

    Add the missing files.

---
+++ js/functions.js
@@ -0,0 +1,142 @@
+function handleSignup(){
+
+    var email = document.getElementById('form-email').value;
+    var password = document.getElementById('form-password').value;
+
+    if(email.length< 4){
+        alert("Please enter a valid email address");
+        return;
+    }
+
+    if(password.length< 4){
+        alert("Please use a stronger password");
+        return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email,password)
+                    .then(
+
+                        function(){
+                            alert("user created");
+                        }
+                        )
+
+                    .catch(
+                        function(error){
+                             alert(error.message);
+                        }
+                        )
+                        
+}
+
+
+function handleSignin(){
+    var email = document.getElementById('form-email').value;
+    var password = document.getElementById('form-password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email,password)
+                .then(
+                    function(){
+                        //window.location.href="http://google.com";
+                        window.location.href="http://mingyanglidesign.com/aau/wnm617/final/home.html";
+                    }
+                    )
+                
+                .catch(
+                    function(error){
+                        alert("wrong username or password");
+                    }
+                    )
+}
+
+function handleSignout(){
+    firebase.auth().signOut()
+    .then(
+        function(){
+            window.location.href="http://mingyanglidesign.com/aau/wnm617/final";
+        }) 
+
+    .catch(
+         function(error){
+            alert(error.message);
+         }
+    )
+
+}
+
+function handleFBlogin(){
+    if(! firebase.auth().currentUser){
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(function(result){
+                var token = result.credential.accessToken;
+                var user = result.user;
+                
+                window.location.href="http://mingyanglidesign.com/aau/wnm617/final/home.html";
+                console.log(user)
+    
+
+            })
+
+            .catch(
+                 function(error){
+                    alert(error.message);
+         }
+    )
+
+
+    }
+
+    else {
+        handleSignout();
+    }
+
+}
+
+
+
+function handlegooglelogin(){
+    if(! firebase.auth().currentUser){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(function(result){
+                var token = result.credential.accessToken;
+                var user = result.user;
+                
+                window.location.href="http://mingyanglidesign.com/aau/wnm617/final/home.html";
+
+            })
+
+            .catch(
+                 function(error){
+                    alert(error.message);
+         }
+    )
+
+
+    }
+
+    else {
+        handleSignout();
+    }
+
+}
+
+
+
+ $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
