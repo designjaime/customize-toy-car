ed281c836131cde471a45bcaf3c6019e2c88790b
jaime <design.jumi@gmail.com>
2018-04-21 00:05:41 -0700

    Use database to load car images.

--- js/home.js
+++ js/home.js
@@ -14,7 +14,63 @@ $(function () {
         $('.back-item').hide();
         $('.item-3').toggle();
     });
-
-    // load car data from database.
-    loadCarsData();
 })
+
+// load cars data from database.
+loadData("cars", function (snapshot) {
+    divStart = '<div class="row justify-content-center">'
+    str = divStart;
+    snapshot.docs.forEach(function (doc, i) {
+        isRightSide = (i + 1) % 2 === 0;
+        isLeftSide = !isRightSide;
+        isLastItem = i === snapshot.docs.length - 1;
+
+        str += isLeftSide ? leftItemDiv(doc.id, doc.data().img) : rightItemDiv(doc.id, doc.data().img);
+
+        if (isLeftSide && isLastItem) {
+            str += '<div class="col"></div>';
+        } else if (isRightSide) {
+            str += '</div>' + divStart;
+        }
+    });
+    str += '</div>';
+    $('#sedan-car-items').append(str);
+});
+
+// load trucks data from database.
+loadData("trucks", function (snapshot) {
+    snapshot.docs.forEach(function (doc) {
+        $('#truck-car-items').append(singleItemDiv(doc.id, doc.data().img));
+    });
+});
+
+// load trucks data from database.
+loadData("jeeps", function (snapshot) {
+    snapshot.docs.forEach(function (doc) {
+        $('#suv-car-items').append(singleItemDiv(doc.id, doc.data().img));
+    });
+});
+
+function leftItemDiv(docId, imgUrl) {
+    return '' +
+        '<div class="col text-left">' +
+        '<a href="car-init.html?id=' + docId + '">' +
+        '    <img class="img-fluid" src="' + imgUrl + '"> </a>' +
+        '</div>';
+}
+
+function rightItemDiv(docId, imgUrl) {
+    return '' +
+        '<div class="col text-right">' +
+        '<a href="car-init.html?id=' + docId + '">' +
+        '    <img class="img-fluid" src="' + imgUrl + '"> </a>' +
+        '</div>';
+}
+
+function singleItemDiv(docId, imgUrl) {
+    return '' +
+        '<div class="col">' +
+        '<a href="car-init.html?id=' + docId + '">' +
+        '    <img class="img-fluid" src="' + imgUrl + '"> </a>' +
+        '</div>';
+}
