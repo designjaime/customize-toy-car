98cfe8c182897df7e01d56cfab7da3eeaf269563
jaime <design.jumi@gmail.com>
2018-05-08 23:18:59 -0700

    Changed a lot.

--- js/customize.js
+++ js/customize.js
@@ -4,5 +4,15 @@ $(function () {
     var formattedImgNum = ("0" + imgNum).slice(-2);
     document.getElementById("car-img").src = carImgUrl.replace("01.jpg", formattedImgNum + ".jpg");
   });
+
+  $(".car-audio-select").click(function (e) {
+    if ($(this).hasClass('active')) {
+      $(".car-audio-select.active").removeClass('active');
+    } else {
+      $(".car-audio-select.active").removeClass('active');
+      $(this).addClass('active');
+    }
+    return false;
+  });
 });
 
