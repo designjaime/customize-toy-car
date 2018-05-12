b525846513b9f205a29e97db25d382a16f54bc2e
jaime <design.jumi@gmail.com>
2018-05-11 17:34:50 -0700

    change payment.js

--- js/payment.js
+++ js/payment.js
@@ -1,21 +1,23 @@
-Stripe.setPublishableKey('your_Key')
-		
-		var stripeResponseHandler = function(status,response){
-			var $form = $('#payment');
-			if(response.error){
-				$form.find('.payment-errors').text(response.error.message);
-				$form.find('button').prop('disabled', false);
-				
-			}else{
-			 var token = response.id;
-			 alert(token);
-			}
-		}
-		jQuery(function(){
-			$('#payment-form').submit(function(e){
-				var $form = $(this);
-				$form.find('button').prop('disabled', true);
-				Stripe.card.createToken($form,stripeResponseHandler);
-				return false;
-			});
-		});
+Stripe.setPublishableKey('pk_test_rOj6ems6tR2M0Sha4b9XkeSi')
+var stripeResponseHandler = function (status, response) {
+	var $form = $('#payment-form');
+	if (response.error) {
+		$form.find('.payment-errors').text(response.error.message);
+		$form.find('button').prop('disable'.false);
+	}
+	else {
+		var token = response.id;
+		alert(token);
+	}
+}
+
+
+//Submit form to Stripe
+jQuery(function ($) {
+	$('#payment-form').submit(function (e) {
+		var $form = $(this);
+		$form.find('button').prop('disable', true);
+		Stripe.card.createToken($form, stripeResponseHandler);
+		return false;
+	});
+});
